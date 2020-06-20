46c142555790c1ce3af6c3fa7aa76e91bc375b79
adisandi3112002 <adisandi3112002@gmail.com>
2020-06-20 19:46:16 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // jshint esversion: 6
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -8,6 +11,10 @@ app.use(express.static("assets"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/know-more.html", function (req, res) {
+  res.sendFile(__dirname + "/know-more.html");
 });
 
 app.listen(process.env.PORT || 3000, function () {
