1c88fd5b0fd0301a54629c4788088187c4c58bd9
adisandi3112002 <adisandi3112002@gmail.com>
2020-06-20 22:42:05 +0530

    slider updates

--- Ishaan/app.js
+++ Ishaan/app.js
@@ -1,38 +1,46 @@
-window.onload = function() {
-	getCovidStats();
-}
+window.onload = function () {
+  getCovidStats();
+};
 
 function getCovidStats() {
-	fetch('https://api.covid19india.org/state_district_wise.json ')
-	.then(function(resp) { return resp.json() })
-	.then(function(data) {
-		
-        let cases = data.Rajasthan.districtData.Jaipur.confirmed; 
-        document.getElementById('cases').innerHTML = cases.toLocaleString('en');
+  fetch("https://api.covid19india.org/state_district_wise.json ")
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      let cases = data.Rajasthan.districtData.Jaipur.confirmed;
+      document.getElementById("cases").innerHTML = cases.toLocaleString("en");
 
-        let deaths = data.Rajasthan.districtData.Jaipur.active; 
-        document.getElementById('Deaths').innerHTML = deaths.toLocaleString('en');
+      let deaths = data.Rajasthan.districtData.Jaipur.active;
+      document.getElementById("Deaths").innerHTML = deaths.toLocaleString("en");
 
-        let active = data.Rajasthan.districtData.Jaipur.deceased; 
-        document.getElementById('active').innerHTML = active.toLocaleString('en');
-        
-        let recovered = data.Rajasthan.districtData.Jaipur.recovered; 
-        document.getElementById('recovered').innerHTML = recovered.toLocaleString('en');
-        
-        let newCases = data.Rajasthan.districtData.Jaipur.delta.confirmed; 
-        document.getElementById('newcases').innerHTML = newCases.toLocaleString('en');
-        
-        let newdeaths = data.Rajasthan.districtData.Jaipur.delta.deceased; 
-        document.getElementById('newDeaths').innerHTML = newdeaths.toLocaleString('en');    
-        
-        let newrecov = data.Rajasthan.districtData.Jaipur.delta.recovered; 
-        document.getElementById('todayrecov').innerHTML = newrecov.toLocaleString('en');    
-        
-        console.log(data);
+      let active = data.Rajasthan.districtData.Jaipur.deceased;
+      document.getElementById("active").innerHTML = active.toLocaleString("en");
 
+      let recovered = data.Rajasthan.districtData.Jaipur.recovered;
+      document.getElementById("recovered").innerHTML = recovered.toLocaleString(
+        "en"
+      );
 
-	}).catch(function() {
-		console.log("error");
-	})
-	setTimeout(getCovidStats, 5000)
+      let newCases = data.Rajasthan.districtData.Jaipur.delta.confirmed;
+      document.getElementById("newcases").innerHTML = newCases.toLocaleString(
+        "en"
+      );
+
+      let newdeaths = data.Rajasthan.districtData.Jaipur.delta.deceased;
+      document.getElementById("newDeaths").innerHTML = newdeaths.toLocaleString(
+        "en"
+      );
+
+      let newrecov = data.Rajasthan.districtData.Jaipur.delta.recovered;
+      document.getElementById("todayrecov").innerHTML = newrecov.toLocaleString(
+        "en"
+      );
+
+      console.log(data);
+    })
+    .catch(function () {
+      console.log("error");
+    });
+  setTimeout(getCovidStats, 5000);
 }
