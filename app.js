d43a8b5745b6e8638646828252bce077166a4e5f
adisandi3112002 <adisandi3112002@gmail.com>
2020-06-20 18:51:49 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// jshint esversion: 6
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("assets"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running at port 3000");
+});
